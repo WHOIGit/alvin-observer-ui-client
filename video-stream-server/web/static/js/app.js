b15cc286b89f02a0b94660decf1f477b97960a3c
ethanandrews <eandrews@whoi.edu>
2021-07-12 17:38:13 -0400

    add new docker-compose file for local React dev, change default to basic local test setup

--- video-stream-server/web/static/js/app.js
+++ video-stream-server/web/static/js/app.js
@@ -1,29 +1,26 @@
 let stream = new MediaStream();
-
-let suuid = $("#suuid").val();
+let suuid = $('#suuid').val();
 
 let config = {
-  iceServers: [
-    {
-      urls: ["stun:stun.l.google.com:19302"]
-    }
-  ]
+  iceServers: [{
+    urls: ["stun:stun.l.google.com:19302"]
+  }]
 };
 
 const pc = new RTCPeerConnection(config);
 pc.onnegotiationneeded = handleNegotiationNeededEvent;
 
 let log = msg => {
-  document.getElementById("div").innerHTML += msg + "<br>";
-};
+  document.getElementById('div').innerHTML += msg + '<br>'
+}
 
 pc.ontrack = function(event) {
   stream.addTrack(event.track);
   videoElem.srcObject = stream;
-  log(event.streams.length + " track is delivered");
-};
+  log(event.streams.length + ' track is delivered')
+}
 
-pc.oniceconnectionstatechange = e => log(pc.iceConnectionState);
+pc.oniceconnectionstatechange = e => log(pc.iceConnectionState)
 
 async function handleNegotiationNeededEvent() {
   let offer = await pc.createOffer();
@@ -32,9 +29,10 @@ async function handleNegotiationNeededEvent() {
 }
 
 $(document).ready(function() {
-  $("#" + suuid).addClass("active");
+  $('#' + suuid).addClass('active');
   getCodecInfo();
 });
+
 
 function getCodecInfo() {
   $.get("../codec/" + suuid, function(data) {
@@ -43,25 +41,11 @@ function getCodecInfo() {
     } catch (e) {
       console.log(e);
     } finally {
-      $.each(data, function(index, value) {
+      $.each(data,function(index,value){
         pc.addTransceiver(value.Type, {
-          direction: "sendrecv"
-        });
-      });
-      //send ping becouse PION not handle RTCSessionDescription.close()
-      sendChannel = pc.createDataChannel("foo");
-      sendChannel.onclose = () => console.log("sendChannel has closed");
-      sendChannel.onopen = () => {
-        console.log("sendChannel has opened");
-        sendChannel.send("ping");
-        setInterval(() => {
-          sendChannel.send("ping");
-        }, 1000);
-      };
-      sendChannel.onmessage = e =>
-        log(
-          `Message from DataChannel '${sendChannel.label}' payload '${e.data}'`
-        );
+          'direction': 'sendrecv'
+        })
+      })
     }
   });
 }
@@ -69,23 +53,17 @@ function getCodecInfo() {
 let sendChannel = null;
 
 function getRemoteSdp() {
-  $.post(
-    "../receiver/" + suuid,
-    {
-      suuid: suuid,
-      data: btoa(pc.localDescription.sdp)
-    },
-    function(data) {
-      try {
-        pc.setRemoteDescription(
-          new RTCSessionDescription({
-            type: "answer",
-            sdp: atob(data)
-          })
-        );
-      } catch (e) {
-        console.warn(e);
-      }
+  $.post("../receiver/"+ suuid, {
+    suuid: suuid,
+    data: btoa(pc.localDescription.sdp)
+  }, function(data) {
+    try {
+      pc.setRemoteDescription(new RTCSessionDescription({
+        type: 'answer',
+        sdp: atob(data)
+      }))
+    } catch (e) {
+      console.warn(e);
     }
-  );
+  });
 }
