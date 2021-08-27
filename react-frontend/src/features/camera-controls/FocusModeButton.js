0c9aa958a424e94c79987254a285f75b3354c7cb
ethanandrews <eandrews@whoi.edu>
2021-08-27 09:41:51 -0400

    refactor Focus button to separate component

---
+++ react-frontend/src/features/camera-controls/FocusModeButton.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Button } from "@material-ui/core";
+import CenterFocusStrongIcon from "@material-ui/icons/CenterFocusStrong";
+// local
+import useCameraWebSocket from "../../hooks/useCameraWebSocket";
+import { selectCurrentCamData } from "./cameraControlsSlice";
+import { COMMAND_STRINGS, NEW_CAMERA_COMMAND_EVENT } from "../../config.js";
+
+const FocusModeButton = () => {
+  const camData = useSelector(selectCurrentCamData);
+  const { messages, sendMessage } = useCameraWebSocket(
+    NEW_CAMERA_COMMAND_EVENT
+  );
+
+  const handleSendMessage = commandName => {
+    let commandValue;
+    if (camData.currentSettings.focus_mode === COMMAND_STRINGS.focusAF) {
+      commandValue = COMMAND_STRINGS.focusMF;
+    } else {
+      commandValue = COMMAND_STRINGS.focusAF;
+    }
+
+    const payload = {
+      action: {
+        name: commandName,
+        value: commandValue
+      }
+    };
+    sendMessage(payload);
+  };
+
+  return (
+    <Button
+      variant="contained"
+      color="secondary"
+      size="small"
+      startIcon={<CenterFocusStrongIcon />}
+      onClick={() => handleSendMessage(COMMAND_STRINGS.focusModeCommand)}
+    >
+      Focus AF/MF
+    </Button>
+  );
+};
+
+export default FocusModeButton;
