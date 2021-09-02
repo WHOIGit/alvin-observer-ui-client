4a41d8ea4ea332b951dd32f5887c9f17063e6afe
ethanandrews <eandrews@whoi.edu>
2021-09-02 15:17:09 -0400

    refactor minivids to separate component, add buttons

---
+++ react-frontend/src/features/pilot-ui/MiniVideo.js
@@ -0,0 +1,104 @@
+import React, { useEffect, useRef } from "react";
+import { useSelector } from "react-redux";
+import adapter from "webrtc-adapter";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Grid,
+  Card,
+  CardHeader,
+  CardMedia,
+  CardActions,
+  CardContent,
+  Typography,
+  Button,
+  Chip
+} from "@material-ui/core";
+// local import
+import useCameraWebSocket from "../../hooks/useCameraWebSocket";
+import WebRtcPlayer from "../../utils/webrtcplayer";
+import { selectActiveCamera } from "../camera-controls/cameraControlsSlice";
+import { VIDEO_STREAM_CONFIG } from "../../config.js";
+
+WebRtcPlayer.setServer(VIDEO_STREAM_CONFIG.server);
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  headerRoot: { padding: "4px" },
+  title: {
+    fontSize: ".9em"
+  },
+  inactiveVideo: {
+    border: "white solid 2px"
+  },
+  activeVideo: {
+    border: "red solid 2px"
+  },
+  videoAction: {
+    justifyContent: "center",
+    textTransform: "uppercase",
+    padding: "4px"
+  },
+  activeVideoAction: {
+    backgroundColor: "red"
+  },
+  cardContent: {
+    padding: 0
+  }
+}));
+
+export default function MiniVideo({ videoSrc, recording }) {
+  const classes = useStyles();
+  const videoElem = useRef(null);
+
+  console.log(videoSrc);
+
+  useEffect(() => {
+    const video = videoElem.current;
+    if (videoSrc) {
+      const playerVideo = new WebRtcPlayer(video.id, videoSrc);
+    }
+  }, [videoSrc]);
+
+  let title;
+  let footer;
+  if (recording) {
+    title = "REC:";
+    footer = "RECORDING";
+  } else {
+    title = "OBS:";
+    footer = "SOURCE";
+  }
+
+  return (
+    <Card className={`${classes.root}`}>
+      <CardHeader
+        title={title}
+        classes={{
+          root: classes.headerRoot,
+          title: classes.title
+        }}
+      />
+      <CardContent className={classes.cardContent}>
+        <div>
+          <video
+            style={{ width: "100%" }}
+            ref={videoElem}
+            autoPlay
+            muted
+          ></video>
+        </div>
+      </CardContent>
+
+      <CardActions
+        className={`${classes.videoAction} ${recording &&
+          classes.activeVideoAction}`}
+      >
+        <Typography variant="body2" color="textSecondary" component="span">
+          {footer}
+        </Typography>
+      </CardActions>
+    </Card>
+  );
+}
--- react-frontend/src/features/pilot-ui/RouterControlContainer.js
+++ react-frontend/src/features/pilot-ui/RouterControlContainer.js
@@ -1,34 +1,113 @@
 import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Grid, Paper, Icon } from "@material-ui/core";
+import { Grid, Paper, Icon, Button, Box } from "@material-ui/core";
 // local
+import useCameraWebSocket from "../../hooks/useCameraWebSocket";
 import RouterControls from "./RouterControls";
+import MiniVideo from "./MiniVideo";
+import {
+  VIDEO_STREAM_CONFIG,
+  COMMAND_STRINGS,
+  NEW_CAMERA_COMMAND_EVENT
+} from "../../config.js";
+
 const useStyles = makeStyles(theme => ({
   paper: {
     padding: theme.spacing(2),
     textAlign: "center",
     color: theme.palette.text.secondary
+  },
+  rightAlign: {
+    textAlign: "right"
   }
 }));
 
 export default function RouterControlContainer() {
   const classes = useStyles();
 
+  const { sendMessage } = useCameraWebSocket(NEW_CAMERA_COMMAND_EVENT);
+
+  const handleSendMessage = (commandName, commandValue) => {
+    const payload = {
+      action: {
+        name: commandName,
+        value: commandValue
+      }
+    };
+    sendMessage(payload);
+  };
+
   return (
     <>
       <Grid container spacing={2}>
-        <Grid item xs={5}>
-          Grid 1
+        <Grid item xs>
+          <MiniVideo
+            videoSrc={VIDEO_STREAM_CONFIG.portObserverVideo}
+            recording={false}
+          />
         </Grid>
-        <Grid item xs={3}>
-          Grid 2
+        <Grid item xs>
+          <MiniVideo
+            videoSrc={VIDEO_STREAM_CONFIG.portRecordVideo}
+            recording={true}
+          />
         </Grid>
-        <Grid item xs={4}>
-          Grid 3
+
+        <Grid item xs>
+          <MiniVideo
+            videoSrc={VIDEO_STREAM_CONFIG.pilotVideo}
+            recording={false}
+          />
+        </Grid>
+
+        <Grid item xs>
+          <MiniVideo
+            videoSrc={VIDEO_STREAM_CONFIG.stbdObserverVideo}
+            recording={false}
+          />
+        </Grid>
+        <Grid item xs>
+          <MiniVideo
+            videoSrc={VIDEO_STREAM_CONFIG.stbdRecordVideo}
+            recording={true}
+          />
         </Grid>
       </Grid>
 
-      <RouterControls />
+      <Box mt={1.5}>
+        <Grid container justifyContent="center" spacing={2}>
+          <Grid item xs>
+            <Button
+              variant="contained"
+              color="primary"
+              size="small"
+              className={classes.ctrlButton}
+              onClick={() =>
+                handleSendMessage(COMMAND_STRINGS.recordSourceCommand, "PORT")
+              }
+            >
+              Record Port Source
+            </Button>
+          </Grid>
+
+          <Grid item xs className={classes.rightAlign}>
+            <Button
+              variant="contained"
+              color="primary"
+              size="small"
+              className={classes.ctrlButton}
+              onClick={() =>
+                handleSendMessage(COMMAND_STRINGS.recordSourceCommand, "STBD")
+              }
+            >
+              Record STBD Source
+            </Button>
+          </Grid>
+        </Grid>
+      </Box>
+      <Box mt={3}>
+        <RouterControls />
+      </Box>
     </>
   );
 }
