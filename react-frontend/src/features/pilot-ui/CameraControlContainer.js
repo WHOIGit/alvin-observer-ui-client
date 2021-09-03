7685a3238d735efc28e0fb43b08c77ed7dbb9286
ethanandrews <eandrews@whoi.edu>
2021-09-03 08:51:57 -0400

    add CamHeartbeat check and initial camera set

--- react-frontend/src/features/pilot-ui/CameraControlContainer.js
+++ react-frontend/src/features/pilot-ui/CameraControlContainer.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { makeStyles } from "@material-ui/core/styles";
 import { Grid, Paper, Icon, List, ListItem } from "@material-ui/core";
 // local
@@ -8,6 +9,19 @@ import SelectShutterMode from "../camera-controls/SelectShutterMode";
 import SelectIrisMode from "../camera-controls/SelectIrisMode";
 import SelectIsoMode from "../camera-controls/SelectIsoMode";
 import SelectExposureMode from "../camera-controls/SelectExposureMode";
+import useCameraWebSocket from "../../hooks/useCameraWebSocket";
+import {
+  selectWebSocketNamespace,
+  selectInitialCamHeartbeatData,
+  selectActiveCamera,
+  changeActiveCamera
+} from "../camera-controls/cameraControlsSlice";
+
+import {
+  CAM_HEARTBEAT,
+  NEW_CAMERA_COMMAND_EVENT,
+  COMMAND_STRINGS
+} from "../../config";
 
 const useStyles = makeStyles(theme => ({
   paper: {
@@ -19,6 +33,40 @@ const useStyles = makeStyles(theme => ({
 
 export default function CameraControlContainer() {
   const classes = useStyles();
+  const dispatch = useDispatch();
+  // connect to CAM_HEARTBEAT, store current cam parameters in Redux state
+  const { messages } = useCameraWebSocket(CAM_HEARTBEAT);
+  console.log(messages);
+  // connect to newCameraCommand
+  const { sendMessage } = useCameraWebSocket(NEW_CAMERA_COMMAND_EVENT);
+  const activeCamera = useSelector(selectActiveCamera);
+  const initialCamHeartbeat = useSelector(selectInitialCamHeartbeatData);
+
+  const setInitialCamera = () => {
+    dispatch(changeActiveCamera(initialCamHeartbeat));
+
+    // send camera change command to set available settings options
+    const payload = {
+      camera: initialCamHeartbeat.camera,
+      action: {
+        name: COMMAND_STRINGS.cameraChangeCommand,
+        value: initialCamHeartbeat.camera
+      }
+    };
+    sendMessage(payload);
+  };
+
+  // use CAM_HEARTBEAT parameters only on initial app load to set activeCamera
+  // keep camera params in local state otherwise
+  useEffect(() => {
+    // set initial camera state only if activeCamera is undefined
+    if (activeCamera === null) {
+      console.log(initialCamHeartbeat);
+      if (initialCamHeartbeat !== null) {
+        setInitialCamera();
+      }
+    }
+  }, [activeCamera, setInitialCamera, initialCamHeartbeat]);
 
   return (
     <>
