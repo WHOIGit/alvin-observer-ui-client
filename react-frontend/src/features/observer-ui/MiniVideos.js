cd7700548c17a9a8bd4a5cc378284cad7dc666af
ethanandrews <eandrews@whoi.edu>
2021-10-18 16:31:43 -0400

    add icons, conditional classes for REC card

--- react-frontend/src/features/observer-ui/MiniVideos.js
+++ react-frontend/src/features/observer-ui/MiniVideos.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
+import clsx from "clsx";
 import { makeStyles } from "@material-ui/core/styles";
 import { Grid, Card, CardHeader, CardContent } from "@material-ui/core";
+import VideocamIcon from "@material-ui/icons/Videocam";
+import VideocamOffIcon from "@material-ui/icons/VideocamOff";
 // local import
 import useCameraWebSocket from "../../hooks/useCameraWebSocket";
 import TopCameraCommandsList from "./TopCameraCommandsList";
@@ -19,20 +22,21 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
   },
   headerRoot: {
-    padding: "4px",
+    padding: "0 2px",
   },
-  headerRecorderRoot: {
-    padding: "4px",
+  headerRecording: {
     backgroundColor: "red",
   },
   title: {
     fontSize: ".9em",
   },
-  inactiveVideo: {
-    border: "white solid 2px",
+  cardAction: {
+    marginTop: "0",
+    marginRight: 0,
+    height: "30px",
   },
-  activeVideo: {
-    border: "red solid 2px",
+  actionIcon: {
+    position: "absolute",
   },
   cardContent: {
     padding: 0,
@@ -47,6 +51,8 @@ export default function MiniVideos({ showFullCameraControls }) {
   //const activeCamera = useSelector(selectActiveCamera);
   const activeCameraConfig = useSelector(selectActiveCameraConfig);
   const { messages } = useCameraWebSocket(RECORDER_HEARTBEAT);
+  console.log(messages);
+  let testVar = "true";
   const videoElemRecord = useRef(null);
   const videoElemObserver = useRef(null);
   const observerVideoSrc = useSelector(
@@ -55,6 +61,11 @@ export default function MiniVideos({ showFullCameraControls }) {
   const recordVideoSrc = useSelector(
     (state) => state.cameraControls.recordVideoSrc
   );
+
+  const cardHeaderStyle = clsx({
+    [classes.headerRoot]: true, //always applies
+    [classes.headerRecording]: messages && testVar === "true", //only when condition === true
+  });
 
   useEffect(() => {
     const videoObserver = videoElemObserver.current;
@@ -84,10 +95,11 @@ export default function MiniVideos({ showFullCameraControls }) {
           <Card className={`${classes.root}`}>
             <CardHeader
               title={messages && `REC: ${messages.camera}`}
+              action={<div>{messages && <VideocamIcon />}</div>}
               classes={{
-                root:
-                  messages && messages.recording && classes.headerRecorderRoot,
+                root: cardHeaderStyle,
                 title: classes.title,
+                action: classes.cardAction,
               }}
             />
             <CardContent className={classes.cardContent}>
@@ -113,9 +125,11 @@ export default function MiniVideos({ showFullCameraControls }) {
                   title={
                     activeCameraConfig && `OBS: ${activeCameraConfig.cam_name}`
                   }
+                  action={<div></div>}
                   classes={{
                     root: classes.headerRoot,
                     title: classes.title,
+                    action: classes.cardAction,
                   }}
                 />
                 <CardContent className={classes.cardContent}>
