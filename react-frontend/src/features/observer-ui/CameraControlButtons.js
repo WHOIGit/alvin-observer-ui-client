9f1d2a91e52d2c662f92cd795056eb63fa3c192f
ethanandrews <eandrews@whoi.edu>
2022-05-09 14:00:22 -0400

    add control/owner checks to Focus/Joystick area

--- react-frontend/src/features/observer-ui/CameraControlButtons.js
+++ react-frontend/src/features/observer-ui/CameraControlButtons.js
@@ -1,31 +1,46 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { makeStyles } from "@material-ui/core/styles";
 import { Box, Divider } from "@material-ui/core";
 
 import FocusModeButton from "../camera-controls/FocusModeButton";
 import FocusZoomButtons from "../camera-controls/FocusZoomButtons";
 import Joystick from "../camera-controls/Joystick";
+import { selectCamHeartbeatData } from "../camera-controls/cameraControlsSlice";
+import useIsOwner from "../../hooks/useIsOwner";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     //flexGrow: 1
-  }
+  },
 }));
 
 export default function CameraControlButtons() {
   const classes = useStyles();
+  const camSettings = useSelector(selectCamHeartbeatData);
+  const { isOwner } = useIsOwner();
+
+  // check to make sure camera has controls and current Observer matches Cam Owner
+  if (camSettings === null || !isOwner) {
+    return null;
+  }
 
   return (
     <div className={classes.root}>
-      <Box my={1}>
-        <FocusModeButton />
-      </Box>
+      {camSettings?.camctrl === "y" && (
+        <>
+          <Box my={1}>
+            <FocusModeButton />
+          </Box>
 
-      <Box my={3}>
-        <FocusZoomButtons />
-      </Box>
+          <Box my={3}>
+            <FocusZoomButtons />
+          </Box>
+        </>
+      )}
+
       <Divider />
-      <Joystick />
+      {camSettings?.pantilt === "y" && <Joystick />}
     </div>
   );
 }
