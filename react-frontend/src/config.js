21b4cffb568c0214c70cca1b1ef508655f10f7f3
Ryan Govostes <rgovostes@whoi.edu>
2022-05-17 18:27:48 +0000

    modified captureIntervalOptions values - mjs

--- react-frontend/src/config.js
+++ react-frontend/src/config.js
@@ -59,16 +59,13 @@ export const COMMAND_STRINGS = {
   captureIntervalCommand: "SIC",
   captureIntervalOptions: [
     "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
     "10",
+    "15",
+    "20",
+    "30",
+    "40",
+    "50",
+    "60",
   ],
 };
 
