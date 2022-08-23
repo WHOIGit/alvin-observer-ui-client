183900c67de6b1d011368b6acf0941bcb25a543c
ethanandrews <eandrews@whoi.edu>
2022-08-23 14:20:52 -0400

    refactor mini video header to new component

---
+++ react-frontend/src/features/observer-ui/MiniVideoHeader.js
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import { CardHeader } from "@material-ui/core";
+import VideocamIcon from "@material-ui/icons/Videocam";
+import VideocamOffIcon from "@material-ui/icons/VideocamOff";
+// local import
+import {
+  selectActiveCameraConfig,
+  selectRecorderResponseError,
+} from "../camera-controls/cameraControlsSlice";
+import useCameraWebSocket from "../../hooks/useCameraWebSocket";
+import { RECORDER_HEARTBEAT, CAM_HEARTBEAT } from "../../config.js";
+
+const useStyles = makeStyles((theme) => ({
+  headerRoot: {
+    padding: "0 2px",
+  },
+  headerRecording: {
+    backgroundColor: "red",
+  },
+  title: {
+    fontSize: ".9em",
+  },
+  cardAction: {
+    marginTop: "0",
+    marginRight: 0,
+    height: "30px",
+  },
+  actionIcon: {
+    position: "absolute",
+  },
+}));
+
+export default function MiniVideoHeader({ videoType }) {
+  // set websocket event based on videoType
+  let wsEvent;
+  if (videoType === "OBS") {
+    wsEvent = CAM_HEARTBEAT;
+  } else if (videoType === "REC") {
+    wsEvent = RECORDER_HEARTBEAT;
+  }
+  const classes = useStyles();
+  const [cameraName, setCameraName] = useState(null);
+  const [isRecording, setIsRecording] = useState(false);
+  const activeCameraConfig = useSelector(selectActiveCameraConfig);
+  const recorderResponseError = useSelector(selectRecorderResponseError);
+  const { messages } = useCameraWebSocket(wsEvent);
+  console.log(wsEvent, messages);
+  const cardHeaderStyle = clsx({
+    [classes.headerRoot]: true, //always applies
+    [classes.headerRecording]: messages && isRecording, //only when condition === true
+  });
+
+  useEffect(() => {
+    if (videoType === "REC" && messages) {
+      setCameraName(messages.camera);
+      setIsRecording(messages.recording === "true");
+    } else if (videoType === "OBS" && activeCameraConfig) {
+      setCameraName(activeCameraConfig.cam_name);
+      setIsRecording(false);
+    }
+  }, [messages, videoType, activeCameraConfig]);
+
+  let title = videoType + ": " + cameraName;
+
+  return (
+    <CardHeader
+      title={title}
+      action={
+        <div>
+          {isRecording && videoType === "REC" && <VideocamIcon />}
+          {!isRecording && videoType === "REC" && <VideocamOffIcon />}
+        </div>
+      }
+      classes={{
+        root: cardHeaderStyle,
+        title: classes.title,
+        action: classes.cardAction,
+      }}
+    />
+  );
+}
