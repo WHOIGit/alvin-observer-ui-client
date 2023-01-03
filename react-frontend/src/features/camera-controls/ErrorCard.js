e1a311aed446d03602cffb8ccec55695829263dc
ethanandrews <eandrews@whoi.edu>
2023-01-03 15:04:19 -0500

    switch icon to error

--- react-frontend/src/features/camera-controls/ErrorCard.js
+++ react-frontend/src/features/camera-controls/ErrorCard.js
@@ -2,7 +2,7 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { red } from "@material-ui/core/colors";
 import { Card, CardContent, Chip, Box } from "@material-ui/core";
-import NoPhotographyIcon from "@mui/icons-material/NoPhotography";
+import ErrorIcon from "@material-ui/icons/Error";
 // local import
 
 const useStyles = makeStyles((theme) => ({
@@ -22,14 +22,10 @@ export default function ErrorCard({ errorType }) {
   return (
     <Card className={`${classes.root}`}>
       <CardContent>
-        <Chip
-          label="CAMERA CHANGE ERROR"
-          className={classes.chip}
-          color="warning"
-        />
+        <Chip label="CAMERA CHANGE ERROR" className={classes.chip} />
       </CardContent>
       <Box m={1}>
-        <NoPhotographyIcon style={{ color: red[400] }} />
+        <ErrorIcon style={{ color: red[400] }} />
       </Box>
     </Card>
   );
