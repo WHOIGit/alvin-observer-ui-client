cc76f37a4dcd7c71e7804fdd6485ab687f4e5d3c
Ryan Govostes <rgovostes@whoi.edu>
2026-05-18 19:22:14 -0400

    Fix RecorderHeartbeatListener namespace

--- react-frontend/src/features/listeners/RecorderHeartbeatListener.jsx
+++ react-frontend/src/features/listeners/RecorderHeartbeatListener.jsx
@@ -1,11 +1,20 @@
-import React, { useCallback } from "react";
-import { useDispatch } from "react-redux";
+import React, { useCallback, useMemo } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useSocketListener } from "../../hooks/useSocket";
 import { RECORDER_HEARTBEAT } from "../../config";
-import { changeRecorderHeartbeat } from "../camera-controls/cameraControlsSlice";
+import {
+  changeRecorderHeartbeat,
+  selectObserverSide,
+} from "../camera-controls/cameraControlsSlice";
+import { getObserverInfo } from "../../utils/observerSide";
 
 export default function RecorderHeartbeatListener({ isEnabled = true }) {
   const dispatch = useDispatch();
+  const observerSide = useSelector(selectObserverSide);
+  const namespaceInfo = useMemo(
+    () => getObserverInfo(observerSide),
+    [observerSide]
+  );
 
   const handleMessage = useCallback(
     (message) => {
@@ -14,7 +23,11 @@ export default function RecorderHeartbeatListener({ isEnabled = true }) {
     [dispatch]
   );
 
-  useSocketListener("/", RECORDER_HEARTBEAT, handleMessage);
+  useSocketListener(
+    namespaceInfo.namespacePath,
+    RECORDER_HEARTBEAT,
+    handleMessage
+  );
 
   return null;
 }
