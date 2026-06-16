c1658569447e5d441805a527eb72d3d116ff9993
Ryan Govostes <rgovostes@whoi.edu>
2026-06-16 17:01:53 -0300

    Build sourcemaps and unminified code

--- react-frontend/vite.config.mjs
+++ react-frontend/vite.config.mjs
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './'
+  base: './',
+  build: {
+    sourcemap: true,
+    minify: false
+  }
 })
