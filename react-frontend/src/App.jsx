d5354e9853303958f2fb53c2aa56d2314f949b98
Nathan Figueroa <nathan.figueroa@whoi.edu>
2026-06-17 16:33:12 -0400

    Move from topbar to more subtle notification bell. Add pilot tab

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -2,13 +2,11 @@ import AppPilot from "./AppPilot.jsx";
 import AppObserver from "./AppObserver.jsx";
 import SystemMessageListener from "./features/listeners/SystemMessageListener.jsx";
 import { WebRtcProvider } from "./features/camera-controls/WebRtcProvider.jsx";
-import SystemNotificationsBar from "./features/system-messages/SystemNotificationsBar.jsx";
 
 export default function App() {
   return (
     <>
       <SystemMessageListener />
-      <SystemNotificationsBar />
       <WebRtcProvider>
         {window.PILOT_MODE === true ? <AppPilot /> : <AppObserver />}
       </WebRtcProvider>
